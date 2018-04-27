dc871c131209b5ae4700a5acc122c03830af16e6
mawlee <36260311+mawlee@users.noreply.github.com>
2018-04-27 13:36:39 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title>HNG</title>
+</head>
+<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+<div>
+<p id="firstId">Hello world</p>
+<p class="first">my first figma design!!!</p>
+<p class="first">hng internship 4</p>
+</div>
+
+<h1>
+<?php
+echo gmdate('Y M j  H:i:s');
+?>
+</h1>
+
+</body>
+</html>
+  
